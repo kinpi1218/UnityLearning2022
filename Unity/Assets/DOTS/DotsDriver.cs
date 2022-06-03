669930cb6cdc0fbad428f0c75be498df6691acf6
Tomonori.Kaneko <kinpi.1218.dev@gmail.com>
2022-06-03 18:02:38 +0900

    jobsystem: parallelFor

--- Unity/Assets/DOTS/DotsDriver.cs
+++ Unity/Assets/DOTS/DotsDriver.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
@@ -57,8 +58,38 @@ namespace Knpl.Dots
             result.Dispose();
             Debug.Log($"[{Time.frameCount}] Job End");
         }
+
+        public void Execute02()
+        {
+            Debug.Log($"[{Time.frameCount}] Job Start");
+            var a = new NativeArray<float>(2, Allocator.TempJob);
+            var b = new NativeArray<float>(2, Allocator.TempJob);
+            var result = new NativeArray<float>(2, Allocator.TempJob);
+
+            a[0] = 1.1f;
+            b[0] = 2.2f;
+            a[1] = 3.3f;
+            b[1] = 4.4f;
+
+            var jobData = new MyParallelJob
+            {
+                a = a,
+                b = b,
+                result = result
+            };
+
+            var handle = jobData.Schedule(result.Length, 1);
+            handle.Complete();
+            Debug.Log($"[{Time.frameCount}] {result[0]}, {result[1]}");
+
+            a.Dispose();
+            b.Dispose();
+            result.Dispose();
+            Debug.Log($"[{Time.frameCount}] Job End");
+        }
     }
 
+    [BurstCompile]
     public struct MyJob : IJob
     {
         public float a;
@@ -71,6 +102,7 @@ namespace Knpl.Dots
         }
     }
 
+    [BurstCompile]
     public struct AddOneJob : IJob
     {
         public NativeArray<float> result;
@@ -79,5 +111,29 @@ namespace Knpl.Dots
         {
             result[0] = result[0] + 1;
         }
+    }
+
+    [BurstCompile]
+    public struct IncrementByDeltaTimeJob : IJobParallelFor
+    {
+        public NativeArray<float> values;
+        public float deltaTime;
+
+        public void Execute(int index)
+        {
+            var tmp = values[index];
+            tmp += deltaTime;
+            values[index] = tmp;
+        }
+    }
+
+    [BurstCompile]
+    public struct MyParallelJob : IJobParallelFor
+    {
+        [ReadOnly] public NativeArray<float> a;
+        [ReadOnly] public NativeArray<float> b;
+        public NativeArray<float> result;
+
+        public void Execute(int i) => result[i] = a[i] + b[i];
     }
 }
